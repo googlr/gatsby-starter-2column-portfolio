bac9905fece23ac7ef268a7ed547647cdccfe0e2
googlr <xg626@nyu.edu>
2018-04-18 23:20:38 -0400

    fix links in sidebar

--- src/components/Sidebar/Links/index.js
+++ src/components/Sidebar/Links/index.js
@@ -9,22 +9,22 @@ class Links extends Component {
       <div className="links">
         <ul className="icons-list">
           <li className="icon">
-            <a href="https://www.github.com/praagyajoshi" target="_blank">
+            <a href="https://www.github.com/googlr" target="_blank">
               <i className="fa fa-github"></i>
             </a>
           </li>
           <li className="icon">
-            <a href="https://www.twitter.com/praagyajoshi" target="_blank">
+            <a href="" target="_blank">
               <i className="fa fa-twitter"></i>
             </a>
           </li>
           <li className="icon">
-            <a href="https://www.linkedin.com/in/praagyajoshi" target="_blank">
+            <a href="https://www.linkedin.com/in/xiujia-guo-88697181/" target="_blank">
               <i className="fa fa-linkedin"></i>
             </a>
           </li>
           <li className="icon">
-            <a href="mailto:hello@praagya.com" target="_blank">
+            <a href="mailto:xg626@nyu.edu" target="_blank">
               <i className="fa fa-envelope"></i>
             </a>
           </li>
